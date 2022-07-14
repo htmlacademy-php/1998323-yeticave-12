ee4b591005fac75d0fc54c47d8f681878792876c
andna <a.n.d.n.a@yandex.ru>
2022-07-14 20:52:03 +0300

    2.5. Итерация по массивам с разными способами обхода массивов
    Поправила)

--- index.php
+++ index.php
@@ -113,20 +113,18 @@ $user_name = 'Natasha'; // укажите здесь ваше имя
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
             <?php
-            $index = 0;
-            $num = count($mmas_ad);
-            while ($index < $num): ?>
+            foreach ($mmas_ad as $product):?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $mmas_ad[$index]['img_url'] ?>" width="350" height="260" alt="">
+                    <img src="<?= $product['img_url'] ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $mmas_ad[$index]['category'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $mmas_ad[$index]['name'] ?></a></h3>
+                    <span class="lot__category"><?= $product['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $product['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $mmas_ad[$index]['price'] ?><b class="rub">р</b></span>
+                            <span class="lot__amount">Стартовая цена: </span>
+                            <span class="lot__cost"><?= $product['price'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -134,8 +132,7 @@ $user_name = 'Natasha'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
-            <?php $index++; ?>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -146,14 +143,11 @@ $user_name = 'Natasha'; // укажите здесь ваше имя
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
             <?php
-            $index = 0;
-            $num = count($mmas_ad);
-            while ($index < $num): ?>
+            for ($index = 0; $index < count($mas_category); $index++): ?>
             <li class="nav__item">
                 <a href="pages/all-lots.html"><?= $mas_category[$index] ?></a>
             </li>
-            <?php $index++; ?>
-            <?php endwhile; ?>
+            <?php endfor; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
