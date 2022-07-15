9fc9a423b6bee772d6cc7937fc6b09a650ce308d
andna <a.n.d.n.a@yandex.ru>
2022-07-15 15:33:32 +0300

    2.6. Пишем первую функцию
    с ошибкой

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ $user_name = 'Natasha'; // укажите здесь ваше имя
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
             <?php
-            foreach ($mmas_ad as $product):?>
+            foreach ($mmas_ad as $product): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
                     <img src="<?= $product['img_url'] ?>" width="350" height="260" alt="">
@@ -121,7 +121,16 @@ $user_name = 'Natasha'; // укажите здесь ваше имя
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена: </span>
-                            <span class="lot__cost"><?= $product['price'] ?><b class="rub">р</b></span>
+                            <span class="lot__cost">
+                            <?php 
+                            $productPrice = $product['price']; ?>
+                            <?php function formatPrice($productPrice)
+                            {
+                                $res = number_format($productPrice, $decimals = 0, $decimal_separator = ".", $thousands_separator = " ") . ' ₽';
+                                return $res;
+                            }; ?>
+                            <?=formatPrice($productPrice); ?>
+                            </span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
